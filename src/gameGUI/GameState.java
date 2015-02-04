c23985eb9abe90f7ae53a1ad833f480d63ccd3fd
This PC <This PC@mr7fx>
2015-02-04 15:45:15 -0500

    Added GUI test layout

--- src/gameGUI/GameState.java
+++ src/gameGUI/GameState.java
@@ -11,25 +11,26 @@ import com.jme3.math.Vector3f;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import static mygame.Main.posX;
 
 /**
  *
  * @author This PC
  */
-public class GameState extends AbstractAppState implements ScreenController{
-    
+public class GameState extends AbstractAppState implements ScreenController {
+
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
         //TODO: initialize your AppState, e.g. attach spatials to rootNode
         //this is called on the OpenGL thread after the AppState has been attached
     }
-    
+
     @Override
     public void update(float tpf) {
         //TODO: implement behavior during runtime
     }
-    
+
     @Override
     public void cleanup() {
         super.cleanup();
@@ -40,7 +41,6 @@ public class GameState extends AbstractAppState implements ScreenController{
 
     public void bind(Nifty nifty, Screen screen) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
     }
 
     public void onStartScreen() {
@@ -50,9 +50,12 @@ public class GameState extends AbstractAppState implements ScreenController{
     public void onEndScreen() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-      public void left() {
-    Vector3f v = mygame.Main.ninja.getLocalTranslation();
-                mygame.Main.ninja.setLocalTranslation(v.x - 2, v.y, v.z);
-                mygame.Main.posX -= 2;
-  }
+
+    public void left() {
+        if (posX > -2) {
+            Vector3f v = mygame.Main.ninja.getLocalTranslation();
+            mygame.Main.ninja.setLocalTranslation(v.x - 2, v.y, v.z);
+            mygame.Main.posX -= 2;
+        }
+    }
 }
